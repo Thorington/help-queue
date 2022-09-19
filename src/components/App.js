e589c928857d081ac5e54a613a35266f5e5bfcb5
Claire Thorington <claire.thorington@gmail.com>
2022-09-19 11:30:04 -0700

    WIP commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import TicketControl from "./TicketControl";
+
+function App(){
+  return(
+    <React.Fragment>
+      <Header />
+      <TicketControl />
+      <hr/>
+    </React.Fragment>
+  )
+}
+
+export default App;
+
